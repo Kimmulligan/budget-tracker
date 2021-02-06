43c573f8b5a7b2075f5200980bfc9918e3914f8b
Kim Mulligan <kimmariemulligan@gmail.com>
2021-02-05 20:31:20 -0800

    added a readme app working

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,73 +1,69 @@
-
-
 let db;
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open("budget_tracker", 1);
 // this happens if the db version changes
-request.onupgradeneeded = function(event) {
-    //save reference to db
-    const db = event.target.result;
-    //create object store (table) with auto-incrementing primary key
-    db.createObjectStore('new_entry', { autoIncrement: true });
+request.onupgradeneeded = function (event) {
+  //save reference to db
+  const db = event.target.result;
+  //create object store (table) with auto-incrementing primary key
+  db.createObjectStore("new_entry", { autoIncrement: true });
 };
-request.onsuccess = function(event) {
-    //save reference to db in global variable when db and object store are succesfully created
-    db = event.target.result;
-    //check if app is online
-    if (navigator.onLine) {
-        uploadEntry();
-    }
+request.onsuccess = function (event) {
+  //save reference to db in global variable when db and object store are succesfully created
+  db = event.target.result;
+  //check if app is online
+  if (navigator.onLine) {
+    uploadEntry();
+  }
 };
-request.onerror = function(event) {
-    console.log(event.target.errorCode);
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
 };
 function saveRecord(record) {
-    //open a new transaction with the db w/ read/write permissions
-    const transaction = db.transaction(['new_entry'], 'readwrite');
-    //access `new_entry` object store
-    const entryObjectStore = transaction.objectStore('new_entry');
-    //add record to object store with add method
-    entryObjectStore.add(record);
+  //open a new transaction with the db w/ read/write permissions
+  const transaction = db.transaction(["new_entry"], "readwrite");
+  //access `new_entry` object store
+  const entryObjectStore = transaction.objectStore("new_entry");
+  //add record to object store with add method
+  entryObjectStore.add(record);
 }
 
-    function uploadEntry() {
-        const transaction = db.transaction(['new_entry'], 'readwrite'); 
+function uploadEntry() {
+  const transaction = db.transaction(["new_entry"], "readwrite");
 
-        const entryObjectStore = transaction.objectStore('new_entry');
+  const entryObjectStore = transaction.objectStore("new_entry");
 
-        const getAll = entryObjectStore.getAll();
+  const getAll = entryObjectStore.getAll();
 
-            getAll.onsuccess = function() {
-                if  (getAll.result.length > 0) {
-                    fetch('/api/transaction/bulk', {
-                        method: 'POST',
-                        body: JSON.stringify(getAll.result),
-                        headers: {
-                            Accept: 'application/json, text/plain, */*',
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    .then(response => response.json())
-                    .then(serverResponse => {
-                        if(serverResponse.message) {
-                            throw new Error(serverResponse);
-                        }
-                        const transaction = db.transaction(['new_entry'], 'readwrite');
-                        const entryObjectStore = transaction.objectStore('new_entry');
-                        entryObjectStore.clear();
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["new_entry"], "readwrite");
+          const entryObjectStore = transaction.objectStore("new_entry");
+          entryObjectStore.clear();
 
-                        alert('All saved entries have been submitted')
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-                }  
-            }
-            
-        }
-
-        //listen for app coming back online
-
-        window.addEventListener('online', function(){
-            console.log('test');
+          alert("All saved entries have been submitted");
         })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
 
+//listen for app coming back online
+
+window.addEventListener("online", function () {
+  console.log("test");
+});
